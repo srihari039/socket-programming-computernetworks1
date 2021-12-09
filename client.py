d947e0b965144ab4515e98eb9bc371806cc1e39f
srihari39 <74501555+srihari39@users.noreply.github.com>
2021-12-09 22:45:38 +0530

    Updated server and client
    Added file transfer, os support

--- client.py
+++ client.py
@@ -1,14 +1,17 @@
+# importing useful libraries
 import socket
 import threading
 import os
 import argparse
 import time
+import sys
 import pkg_resources
 import subprocess
-import sys
 from tqdm import tqdm
 from PIL import Image
 
+# these are the libraries generally not installed in all the laptops
+# check and install if these libraries are not present
 required = {'pyqrcode', 'tqdm'}
 installed = {pkg.key for pkg in pkg_resources.working_set}
 missing_package = required - installed
@@ -19,101 +22,166 @@ def install(package):
 for package in missing_package:
     install(package)
 
+# argument parser
 parser = argparse.ArgumentParser(description='This is client')
 args = parser.parse_args()
 
-HOST = '192.168.0.18'
-# HOST = '127.0.0.1'
-PORT = 12002
-FORMAT = 'utf-8'
+# Host and Port
+HOST = input('Enter the IP address : ')
+PORT = int(input('Enter the port Number : '))
 ADDRESS = (HOST,PORT)
+
+# create a client and connect with server
+client = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+client.connect(ADDRESS)
+
+# global attributes / key Identifiers
+FORMAT = 'utf-8'
 ASK_NAME = 'Please enter your name : '
-HEADER = 1000*1024*1000
+HEADER = 100*1024
 CLIENT_NAME = input(ASK_NAME)
 MAX_COUNT = 1e5
 REQUEST_FILE = '!req'
 QUIT_CLIENT_MESSAGE = '!exit'
+SEND_FILE = '!send'
 SEPARATOR = '<>'
 QR_CODE = 'qr'
-client = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
-client.connect(ADDRESS)
 
+# utility function to split the name and get attributes from the message
 def split_get(message):
     file_name = message.split(SEPARATOR)[0]
     file_size = int(message.split(SEPARATOR)[1])
     one,two = file_name.split('.')
     file_name = one+'_recieved.'+two
     return file_name,file_size
-    
+
+# function to recieve file from server
+def recieve_file(message):
+    file_name,file_size = split_get(message)
+    print(file_name,file_size)
+    file = open(file_name,'wb')
+    bytes_size = 0
+    with tqdm(total=file_size) as progress_bar:
+        while bytes_size < file_size:
+            contents = client.recv(1024)
+            bytes_size += len(contents)
+            progress_bar.update(len(contents))
+            file.write(contents)
+        file.close()
+    # returning file name for further flow
+    return file_name
+
+# function which listens/recieves to server
 def recieve_from_server():
     count = 0
     while True:
         try:
+            # recieve message from server
             message = client.recv(HEADER)
             message = message.decode(FORMAT)
+            
+            # if it asks name, send the name
             if message == ASK_NAME:
                 client.send(CLIENT_NAME.encode(FORMAT))
+                
+            # if message is to quit, then quit the connection
             elif message == QUIT_CLIENT_MESSAGE:
                 print('[**Terminating**] Terminating connection from server side')
                 print('Connection removed!')
                 os._exit(0)
+            
+            # elif block  with no use--> to be commented
             elif message == '':
                 count += 1
+                
+            # if qr in message, deal accordingly to recieve the file
             elif QR_CODE in message:
-                file_name,file_size = split_get(message)
-                file = open(file_name,'wb')
-                bytes_size = 0
-                while bytes_size < file_size:
-                    contents = client.recv(1024)
-                    bytes_size += len(contents)
-                    file.write(contents)
-                file.close()
+                file_name = recieve_file(message)
+                print(f'QR code recieved successfully')
                 img = Image.open(file_name)
                 img.show()
                 client.send(QR_CODE.encode(FORMAT))
-                
+
+                # starting a thread
+                # a qr must be safe and expire after some time,
+                # so deleting the file after 5 seconds 
                 thread = threading.Thread()
                 thread.start()
-                time.sleep(2)
+                time.sleep(5)
                 print('Closing the qr code')
                 os.remove(file_name)
             
+            # if client to recieve message, use the utility function
+            # and recieve the file
             elif SEPARATOR in message:
-                file_name,file_size = split_get(message)
-                print(file_name,file_size)
-                file = open(file_name,'wb')
-                bytes_size = 0
-                with tqdm(total=file_size) as progress_bar:
-                    while bytes_size < file_size:
-                        contents = client.recv(1024)
-                        bytes_size += len(contents)
-                        progress_bar.update(len(contents))
-                        file.write(contents)
-                    file.close()
-                
+                file_name = recieve_file(message)
                 print(f'File name with {file_name} recieved successfully')
-
+                
+            # else print the message
             else:
                 print(message)
 
+            # to be commented block
             if count > MAX_COUNT:
                 raise Exception
 
         except:
             pass
 
-
+# function which writes to the server
 def write_to_server():
     while True:
-        msg = input('')
-        message = f'{CLIENT_NAME} : {msg}'.encode(FORMAT)
-        original_message = message.decode(FORMAT)
-        client.send(message)
+        try:
+            # take the input and send the message
+            msg = input('')
+            message = f'{CLIENT_NAME} : {msg}'.encode(FORMAT)
+            original_message = message.decode(FORMAT)
+            client.send(message)
 
-        if original_message == f'{CLIENT_NAME} : !exit':
+            # if a file is to be sent to server
+            if SEND_FILE in original_message:
+                try:
+                    # read and send file dividing into fine chunks
+                    file_name = msg.split(' ')[-1]
+                    file_size = os.path.getsize(file_name)
+                    print(f'Sending {file_name} to server')
+                    client.send(f'{file_name}{SEPARATOR}{file_size}'.encode(FORMAT))
+                    time.sleep(0.1)
+                    file = open(file_name,'rb')
+                    bytes_size = 0
+                    contents = 0
+                    with tqdm(total=file_size) as progress_bar:
+                        while bytes_size < file_size:
+                            contents = file.read(1024)
+                            if not contents:
+                                break
+                            bytes_size += len(contents)
+                            progress_bar.update(len(contents))
+                            client.send(contents)
+                        file.close()
+                    print('File sent successfully')
+                except:
+                    print('Unable to open/read file')
+
+            # if message is to exit, then exit from the program
+            if original_message == f'{CLIENT_NAME} : !exit':
+                os._exit(0)
+        
+        # exception for keyboard interrupt
+        except KeyboardInterrupt:
+            # send message to server, to remove the connection
+            message = f'{CLIENT_NAME} : !exit'.encode(FORMAT)
+            client.send(message)
+            # exit the program
+            print('[**Key board interrupt**] Terminating connection')
             os._exit(0)
         
+# we need to run "recieve_from_server" and "write_to_server" simultaneously
+# it is achieved using threads
+
+# thread to start function which recieves responses from server
 recieve_thread = threading.Thread(target=recieve_from_server)
 recieve_thread.start()
 
+# function write to server
 write_to_server()
--- server.py
+++ server.py
@@ -1,14 +1,18 @@
+# importing useful libraries
 import socket
 import threading
-import argparse
 import os
+import argparse
 import time
 import sys
+import pkg_resources
 import subprocess
 import pyqrcode
-import pkg_resources
+from tqdm import tqdm
 from datetime import datetime
 
+# these are the libraries generally not installed in all the laptops
+# check and install if these libraries are not present
 required = {'pyqrcode', 'tqdm'}
 installed = {pkg.key for pkg in pkg_resources.working_set}
 missing_package = required - installed
@@ -19,22 +23,30 @@ def install(package):
 for package in missing_package:
     install(package)
 
+# argument parser
 parser = argparse.ArgumentParser(description='This is server!\n')
-# parser.add_argument('!req',help=' Request file from server\n')
 args = parser.parse_args()
 
+# Host and port
 HOST = '192.168.0.18'
 PORT = 12002
-FORMAT = 'utf-8'
+
+# creating server and binding address to listen
 server = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
 ADDRESS = (HOST,PORT)
-ASK_NAME = 'Please enter your name : '
 server.bind(ADDRESS)
+
+# listen on server
 server.listen()
+
+# global attributes / key Identifiers
+FORMAT = 'utf-8'
+ASK_NAME = 'Please enter your name : '
 QUIT_CLIENT_MESSAGE = '!exit'
 REQUEST_FILE = '!req'
+SEND_FILE = '!send'
 COMMAND = '!cmd'
-HEADER = 1000*1000*1024
+HEADER = 100*1024
 SEPARATOR = '<>'
 clients = []
 names = []
@@ -44,11 +56,16 @@ CLIENT_NAME = 'client.py'
 QR_CODE = 'qr'
 ONLINE_CLIENTS = '!online'
 
+# function to broadcast message
+# sends a particluar message to all clients
+# except the client from whom the message comes
 def broadcast_message(message,flag_client=None):
     for client in clients:
         if client != flag_client:
             client.send(message)
 
+# function to remove client from server
+# closes the client connection 
 def remove_client(client):
     index = clients.index(client)
     clients.remove(client)
@@ -59,21 +76,71 @@ def remove_client(client):
     client.close()
     print(f'Active connections at {str(datetime.now())} : ',len(clients))
 
+# utility function to split messages
+def split_get(message):
+    file_name = message.split(SEPARATOR)[0]
+    file_size = int(message.split(SEPARATOR)[1])
+    one,two = file_name.split('.')
+    file_name = one+'_recieved_to_server.'+two
+    return file_name,file_size    
+
+# function to send files to client
+def send_file(file_name,file_size,client):
+    # open the file with read bytes mode
+    file = open(file_name,'rb')
+    bytes_size = 0
+    contents = 0
+    # read chunks at time and send each one individually
+    with tqdm(total=file_size) as progress_bar:
+        while bytes_size < file_size:
+            contents = file.read(1024)
+            if not contents:
+                break
+            progress_bar.update(len(contents))
+            bytes_size += len(contents)
+            client.send(contents)
+    # close the file
+    file.close() 
+
+# function which handles the client
 def handle_client(client,address):
     print(f'[New connection] Address with {address} connected!')
     print(f'Active connections at {str(datetime.now())} : ',len(clients))
+    
     while True:
         try:
+            # server tries to recieve message
             message = client.recv(HEADER)
             msg = message.decode(FORMAT)
+            
+            # if quit message appears, break and remove the client
             if QUIT_CLIENT_MESSAGE in msg:
                 break
 
+            # if online clients appears in message, lists names of clients online
             elif ONLINE_CLIENTS in msg:
                 names_online = '\n'.join(names)
                 msg = 'Currently online : \n'+names_online+'\n'+10*"*="+'*'
                 client.send(msg.encode(FORMAT))
+    
+            # if send request from client, then deal accordingly
+            # by writing byte format to files
+            elif SEND_FILE in msg:
+                message = client.recv(HEADER).decode(FORMAT)
+                file_name,file_size = split_get(message)
+                print(f'File incoming with name {file_name}')
+                
+                file = open(file_name,'wb')
+                bytes_size = 0
+                while bytes_size < file_size:
+                    contents = client.recv(1024)
+                    bytes_size += len(contents)
+                    file.write(contents)
+                file.close()
+                print('File recived from client')
 
+            # if request file from client, then deal accodingly
+            # by reading and sending to client
             elif REQUEST_FILE in msg:
                 try:
                     file_name = msg.split(' ')[-1].strip()
@@ -81,21 +148,14 @@ def handle_client(client,address):
                     print(f'{client_name} requested file with name : {file_name}')
                     file_size = os.path.getsize(file_name)
                     client.send(f'{file_name}{SEPARATOR}{file_size}'.encode(FORMAT))
-                    file = open(file_name,'rb')
-                    bytes_size = 0
-                    contents = 0
-                    while bytes_size < file_size:
-                        contents = file.read(1024)
-                        if not contents:
-                            break
-                        bytes_size += len(contents)
-                        client.send(contents)
-                    file.close()
+                    send_file(file_name,file_size,client)
                     print('File sent successfully')
                 except:
                     print('Unable to find/open the file')
                     client.send('Unable to find/open the file'.encode(FORMAT))
 
+            # if command is requested by client, then deal accordingly
+            # by giving access properly only to certain commands
             elif COMMAND in msg:
                 try:
                     command = msg.split(':')[-1].split(COMMAND)[-1].strip()
@@ -104,13 +164,13 @@ def handle_client(client,address):
                     print('Command to execute : ',command)
                     print('Command executed and sending output')
                     msg = f'[**Executing**] $ {command} : \n'
-                    result = subprocess.check_output(command).decode(FORMAT)
+                    result = subprocess.check_output(command, shell=True).decode(FORMAT)
                     if 'ls' in command:
                         result = result.split('\n')
                         result.remove(CLIENT_NAME)
                         result.remove(SERVER_NAME)
                         result = '\n'.join(result)
-                    result = msg + result + 10*"*="+'*'
+                    result = msg + result + '\n'
                     client.send(result.encode(FORMAT))
                 except:
                     msg = f'No previliges to run the command:{command}'
@@ -118,6 +178,8 @@ def handle_client(client,address):
                     msg = '[**Warning**] '+msg
                     client.send(msg.encode(FORMAT))
 
+            # if client requests for bank authentication, then
+            # send the qr code file
             elif BANK_AUTHENTICATION in msg:
                 try:
                     bank_url = 'www.myCNbank.org'
@@ -126,16 +188,7 @@ def handle_client(client,address):
                     url.png(file_name,scale=10)
                     file_size = os.path.getsize(file_name)
                     client.send(f'{file_name}{SEPARATOR}{file_size}{SEPARATOR}{QR_CODE}'.encode(FORMAT))
-                    file = open(file_name,'rb')
-                    bytes_size = 0
-                    contents = 0
-                    while bytes_size < file_size:
-                        contents = file.read(1024)
-                        if not contents:
-                            break
-                        bytes_size += len(contents)
-                        client.send(contents)
-                    file.close()
+                    send_file(file_name,file_size,client)
                     print('QR sent successfully')
                     os.remove(file_name)
                 except:
@@ -148,36 +201,51 @@ def handle_client(client,address):
             
     remove_client(client)
     
+# function which deals with all clients
+# establishes a server and connects clients upon request
 def recieve_from_client():
     print(f'**Starting** Starting the server')
     print(f'Server listening at {HOST}')
 
     try:
         while True:
+            # accepting request from client
             client,address = server.accept()
+            print('New connection request incoming!')
             
+            # recording the name of the client
             client.send(ASK_NAME.encode(FORMAT))
             new_name = client.recv(HEADER).decode(FORMAT)
+            
+            # keep the name and clients in a list
+            # to access them further for utility
             names.append(new_name)
             clients.append(client)
             
             print(f'**Connecting** Client connecting with name : {new_name}')
-                    
-            client.send('connected to server!'.encode(FORMAT))
+            
+            client.send('connected to server!\n'.encode(FORMAT))
             broadcast_message(f'{new_name} has joined the chat!'.encode(FORMAT))
             
+            # starting a new thread and assigning it to new client
             thread = threading.Thread(target=handle_client,args=(client,address))
             thread.start()
             
+    # keyboard interrupt exception
     except KeyboardInterrupt:
         print('**[Terminating]** Server terminating')
+        
+        # remove all clients one by one
         while(len(clients)):
             clients[0].send(QUIT_CLIENT_MESSAGE.encode(FORMAT))
             time.sleep(0.1)
             remove_client(clients[0])
+        
+        # shutdown the server and close
         server.shutdown(socket.SHUT_RDWR)
         server.close()
         print ("Server terminated!")
+        # exit the program
         os._exit(0)
 
 recieve_from_client()
